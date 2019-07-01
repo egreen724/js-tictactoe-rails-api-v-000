76203cb2b4038a0bb2e9909553d728bd37c801e3
Erin Green <erin.y.green@gmail.com>
2019-07-01 20:23:54 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -6,6 +6,10 @@ const winCombinations = [
         [0,1,2], [3,4,5], [6,7,8], [0,3,6],[1,4,7], [2,5,8], [0,4,8], [6,4,2]
     ]
 
+$(document).ready(function() {
+  attachListeners();
+});
+
 function player(turn) {
   if (turn % 2 !== 0) {
     return 'O'
@@ -32,38 +36,52 @@ function saveGame(){
   // write this
 }
 
+function previousGames() {
+
+}
+
 function checkWinner() {
   var board = {}
   var winner = false;
 
   winCombinations.some(function(combo) {
     if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
-      debugger; 
       setMessage(`Player ${player(turn)} Won!`);
       return winner = true;
     }
   });
 
   return winner;
-
-  // if (state == winning) {
-  //   true;
-  //   setMessage(`Player ${player(turn)} Won!`);
-  // } else {
-  //     false;
-  //   }
 }
 
 function doTurn(element) {
   turn ++
   updateState(element);
 
-  if (checkWinner()) {
+  // if (checkWinner()) {
+  //   saveGame();
+  //   resetBoard();
+  // } else if (turn === 9) {
+  //   setMessage("Tie Game.");
+  //   saveGame()
+  //   resetBoard()
+  // }
+}
+
+function attachListeners() {
+  $("td").on("click", function(){
+    doTurn(this);
+  })
+
+  $("button#save").on("click", function() {
     saveGame();
-    resetBoard();
-  } else if (turn === 9) {
-    setMessage("Tie Game.");
-    saveGame()
+  })
+
+  $("button#previous").on("click", function() {
+    previousGames();
+  })
+
+  $("button#clear").on("click", function() {
     resetBoard()
-  }
+  })
 }
